bc138c7d6b2a981a4a0b853490011299a6c426b4
José Ángel García <chepeaicrag12@gmail.com>
2022-05-24 23:51:59 -0500

    Added: app users

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from drf_base64.serializers import ModelSerializer
+
+from .models import User
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = ('user_permissions', )
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.image = validated_data["image"]        
+        user.save()
+        return user
+
+class UserListSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = ('is_staff', 'status_delete', 'is_active',
+                   'is_superuser', 'password', 'user_permissions', 'groups')
